1405e25d69018c485273592bc26af1fa9a527192
SMAIGHT\jw <jwjeong@smaight.co.kr>
2025-08-26 15:32:31 +0900

    엔티티 개발 - 연관관계 있음

--- src/main/kotlin/com/pandas/portfolio/domain/entity/ProjectDetail.kt
+++ src/main/kotlin/com/pandas/portfolio/domain/entity/ProjectDetail.kt
@@ -1,18 +1,29 @@
 package com.pandas.portfolio.domain.entity
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import org.hibernate.annotations.Columns
+import jakarta.persistence.*
 
 @Entity
-class ProjectDetail: BaseEntity() {
+class ProjectDetail(
+    content: String,
+    url: String?,
+    isActive: Boolean
+) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "project_detail_id")
     var id: Long? = null
+
+    var content: String = content
+
+    var url: String? = url
+
+    var isActive: Boolean = isActive
+
+    fun update(content: String, url: String?, isActive: Boolean) {
+        this.content = content
+        this.url = url
+        this.isActive = isActive
+    }
 
 }
