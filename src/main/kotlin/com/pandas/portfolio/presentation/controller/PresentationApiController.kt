1d64bdbbde84c11f82ede2328751924eb26dc957
jiwon <panda981018@gmail.com>
2025-10-02 17:21:31 +0900

    컨트롤러 개발

--- src/main/kotlin/com/pandas/portfolio/presentation/controller/PresentationApiController.kt
+++ src/main/kotlin/com/pandas/portfolio/presentation/controller/PresentationApiController.kt
@@ -1,15 +1,42 @@
 package com.pandas.portfolio.presentation.controller
 
+import com.pandas.portfolio.presentation.dto.IntroductionDTO
+import com.pandas.portfolio.presentation.dto.LinkDTO
+import com.pandas.portfolio.presentation.dto.ProjectDTO
+import com.pandas.portfolio.presentation.dto.ResumeDTO
+import com.pandas.portfolio.presentation.service.PresentationService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api")
-class PresentationApiController {
+@RequestMapping("/api/v1")
+class PresentationApiController(
+        private val presentationService: PresentationService
+) {
 
     @GetMapping("/test")
     fun test(): String {
         return "OK"
+    }
+
+    @GetMapping("/introductions")
+    fun getIntroductions(): List<IntroductionDTO> {
+        return presentationService.getIntroductions()
+    }
+
+    @GetMapping("/links")
+    fun getLinks(): List<LinkDTO> {
+        return presentationService.getLinks()
+    }
+
+    @GetMapping("/resume")
+    fun getResume(): ResumeDTO {
+        return presentationService.getResume()
+    }
+
+    @GetMapping("/projects")
+    fun getProjects(): List<ProjectDTO> {
+        return presentationService.getProjects()
     }
 }
