1f2a9bdac12c47a035fb8d779f22bcea931f7b94
jiwon <panda981018@gmail.com>
2025-10-02 16:08:29 +0900

    DTO 개발

--- src/main/kotlin/com/pandas/portfolio/presentation/dto/SkillDTO.kt
+++ src/main/kotlin/com/pandas/portfolio/presentation/dto/SkillDTO.kt
@@ -1,3 +1,13 @@
 package com.pandas.portfolio.presentation.dto
 
-class SkillDTO
+import com.pandas.portfolio.domain.entity.Skill
+
+data class SkillDTO(
+        val name: String,
+        val type: String
+) {
+    constructor(skill: Skill) : this(
+            name = skill.name,
+            type = skill.type.name
+    )
+}
