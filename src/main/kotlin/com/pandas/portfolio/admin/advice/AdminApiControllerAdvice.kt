81100a996a43f7de3b89b20fccfab79209937db4
jiwon <panda981018@gmail.com>
2025-11-16 16:17:33 +0900

    공통 개발 - Exception과 Advice

--- src/main/kotlin/com/pandas/portfolio/admin/advice/AdminApiControllerAdvice.kt
+++ src/main/kotlin/com/pandas/portfolio/admin/advice/AdminApiControllerAdvice.kt
@@ -1,7 +1,38 @@
 package com.pandas.portfolio.admin.advice
 
+import com.pandas.portfolio.admin.exception.AdminException
+import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
-class AdminApiControllerAdvice {
+class AdminApiControllerAdvice { // 예외처리에 특화된 클래스
+
+    val log = LoggerFactory.getLogger(AdminApiControllerAdvice::class.java)
+
+    @ExceptionHandler
+    fun handleException(e: AdminException): ResponseEntity<String> {
+        log.info(e.message, e)
+
+        return ResponseEntity.status(e.httpStatus).body(e.message)
+    }
+
+    @ExceptionHandler
+    fun handleException(e: MethodArgumentNotValidException): ResponseEntity<String> {
+        log.info(e.message, e)
+
+        val fieldError = e.bindingResult.fieldErrors[0] // 어떤 검증에서 오류가 났는지 bindingResult가 들고 있음.
+        val message = "[${fieldError.field} ${fieldError.defaultMessage}]"
+
+        return ResponseEntity.badRequest().body(message)
+    }
+
+    @ExceptionHandler
+    fun handleException(e: Exception): ResponseEntity<String> {
+        log.info(e.message, e)
+
+        return ResponseEntity.internalServerError().body("시스템 오류가 발생했습니다.")
+    }
 }
