a27b2357e51c247e53dc3ce56e2bbafb99f69670
jiwon <panda981018@gmail.com>
2025-11-16 15:30:05 +0900

    인터셉터 개발

--- src/main/kotlin/com/pandas/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/pandas/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,6 +1,19 @@
 package com.pandas.portfolio.presentation.interceptor
 
+import com.pandas.portfolio.domain.entity.HttpInterface
+import com.pandas.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
 
 @Component
-class PresentationInterceptor
+class PresentationInterceptor(
+        private val httpInterfaceRepository: HttpInterfaceRepository
+) : HandlerInterceptor {
+
+    override fun afterCompletion(request: HttpServletRequest, response: HttpServletResponse, handler: Any, ex: Exception?) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
+}
